545659cb4fee22a2766ba4b4e96e1dd5699808a4
Arnav Singhal <singhal.arnav@gmail.com>
2022-05-29 21:08:16 +0530

    Initial Commit

---
+++ AllAds/AllAdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllAdsApp.swift
+//  AllAds
+//
+//  Created by Arnav Singhal on 29/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AllAdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllAds/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AllAds
+//
+//  Created by Arnav Singhal on 29/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

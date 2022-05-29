801694c6b0309e33e8e5105f57f5a6b3501cb82e
Arnav Singhal <singhal.arnav@gmail.com>
2022-05-29 22:55:49 +0530

    Banner ads implemented!

--- AllAds/AllAdsApp.swift
+++ AllAds/AllAdsApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import GoogleMobileAds
 
 @main
 struct AllAdsApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Finish launching!")
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        return true
     }
 }
--- AllAds/ContentView.swift
+++ AllAds/ContentView.swift
@@ -5,12 +5,16 @@
 //  Created by Arnav Singhal on 29/05/22.
 //
 
+
 import SwiftUI
+import GoogleMobileAds
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      VStack {
+        Text("Google Admob")
+        GADBannerViewController()
+      }
     }
 }
 
@@ -18,4 +22,31 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+// MARK: - GADBannerViewController
+struct GADBannerViewController: UIViewControllerRepresentable {
+  
+  func makeUIViewController(context: Context) -> some UIViewController {
+    let view = GADBannerView(adSize: GADAdSizeBanner)
+    let viewController = UIViewController()
+    let testID = "ca-app-pub-3940256099942544/2934735716"
+    let realID = "ca-app-pub-9450475706334809/7473547174"
+    
+    // Banner Ad
+    view.adUnitID = realID
+    view.rootViewController = viewController
+    
+    // View Controller
+    viewController.view.addSubview(view)
+    viewController.view.frame = CGRect(origin: .zero, size: GADAdSizeBanner.size)
+    
+    // Load an ad
+    view.load(GADRequest())
+    
+    return viewController
+  }
+  
+  func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
+  
 }
